fa797e4a5e9a01c184bc6882768bd533621ab2a1
Shanika <shanikacj@gmail.com>
2017-05-25 08:02:47 +1200

    Adde bit of logging

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ const serverConfig = {
 
 // Create a server for the client html page
 var handleRequest = function(request, response) {
+    console.log("Trying to serve : " + request.url);
     if (request.url === '/') {
         response.writeHead(200, { 'Content-Type': 'text/html' });
         response.end(fs.readFileSync('../client/login.html'));
