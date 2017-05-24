9af6f34123d52e0bb7be52a92dc2556a5c1832d1
Shanika <shanikacj@gmail.com>
2017-05-25 08:18:10 +1200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,130 +1,148 @@
-// const HTTPS_PORT = 8000;
-const HTTPS_PORT = process.env.PORT || 3000;
-
-const TYPE_INITIAL_HANDSHAKE = 0;
-const TYPE_SDP_CONNECTION = 1;
-const TYPE_ICE_INFO = 2;
-const TYPE_BITRATE_CHANGED_INFO = 3;
-const TYPE_CHAT_MESSAGE = 4;
-const TYPE_REQUEST_OFFER = 5;
-
-const fs = require('fs');
-const https = require('https');
-const WebSocket = require('ws');
-
-// SSL is required for the WebRTC connections for Chrome. We use a temporary self-signed certificate
-// https://tokbox.com/blog/the-impact-of-googles-new-chrome-security-policy-on-webrtc/
-const serverConfig = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem'),
-};
-
-// Create a server for the client html page
-var handleRequest = function(request, response) {
-    console.log("Trying to serve : " + request.url);
-    if (request.url === '/') {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.end(fs.readFileSync('../client/login.html'));
-    } else if (request.url === '/app.js') {
-        response.writeHead(200, { 'Content-Type': 'application/javascript' });
-        response.end(fs.readFileSync('./client/app.js'));
-    } else if (request.url === '/app') {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.end(fs.readFileSync('./client/index.html'));
-    } else if (request.url === "/style.css") {
-        response.writeHead(200, { 'Content-Type': 'text/css' });
-        response.end(fs.readFileSync('./client/style.css'));
-    } else if (request.url === "/img/slide_01.jpg") {
-        response.writeHead(200, { 'Content-Type': 'image/jpeg' });
-        response.end(fs.readFileSync('./client/img/slide_01.jpg'));
-    } else if (request.url === "/img/slide_02.jpg") {
-        response.writeHead(200, { 'Content-Type': 'image/jpeg' });
-        response.end(fs.readFileSync('./client/img/slide_01.jpg'));
-    } else if (request.url === "/img/logo.png") {
-        response.writeHead(200, { 'Content-Type': 'image/png' });
-        response.end(fs.readFileSync('./client/img/logo.png'));
-    } else if (request.url === "/img/HeaderImg.png") {
-        response.writeHead(200, { 'Content-Type': 'image/png' });
-        response.end(fs.readFileSync('./client/img/HeaderImg.png'));
-    } else if (request.url === "/img/Logo_Clear2.png") {
-        response.writeHead(200, { 'Content-Type': 'image/png' });
-        response.end(fs.readFileSync('./client/img/Logo_Clear2.png'));
-    } else if (request.url === "/face.js") {
-        response.writeHead(200, { 'Content-Type': 'application/javascript' });
-        response.end(fs.readFileSync('./client/lib/face-min.js'));
-    } else if (request.url === "/tracking.js") {
-        response.writeHead(200, { 'Content-Type': 'application/javascript' });
-        response.end(fs.readFileSync('./client/lib/tracking-min.js'));
-    } else {
-        console.log("Unknown path " + request.url);
-        response.writeHead(404);
-        response.end();
-    }
-};
-
-var httpsServer = https.createServer(handleRequest);
-httpsServer.listen(HTTPS_PORT);
-httpsServer.timeout = 4000;
-// Create a server for handling websocket calls
-var wss = new WebSocket.Server({
-    server: httpsServer,
-    clientTracking: true,
+var http = require("http");
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<!DOCTYPE "html">");
+  response.write("<html>");
+  response.write("<head>");
+  response.write("<title>Hello World Page</title>");
+  response.write("</head>");
+  response.write("<body>");
+  response.write("Hello World!");
+  response.write("</body>");
+  response.write("</html>");
+  response.end();
 });
 
-wss.on('connection', function(ws) {
-    ws.on('message', function(message) {
-        var msg = JSON.parse(message);
-        switch (msg.type) {
-            case TYPE_INITIAL_HANDSHAKE:
-                //clients[msg.id] = ws;
-                console.log("New handshake from " + msg.id + " at " + msg.date);
-                break;
+server.listen(80);
+console.log("Server is listening");
 
-            case TYPE_SDP_CONNECTION:
-                wss.broadcast(message); //todo - sent to the correct client 
-                break;
+// // const HTTPS_PORT = 8000;
+// const HTTPS_PORT = process.env.PORT || 3000;
 
-            case TYPE_ICE_INFO:
-                wss.broadcast(message);
-                break;
+// const TYPE_INITIAL_HANDSHAKE = 0;
+// const TYPE_SDP_CONNECTION = 1;
+// const TYPE_ICE_INFO = 2;
+// const TYPE_BITRATE_CHANGED_INFO = 3;
+// const TYPE_CHAT_MESSAGE = 4;
+// const TYPE_REQUEST_OFFER = 5;
 
-            case TYPE_BITRATE_CHANGED_INFO:
-                wss.broadcast(message);
-                break;
+// const fs = require('fs');
+// const https = require('https');
+// const WebSocket = require('ws');
 
-            case TYPE_CHAT_MESSAGE:
-                wss.broadcast(message);
-                break;
+// // SSL is required for the WebRTC connections for Chrome. We use a temporary self-signed certificate
+// // https://tokbox.com/blog/the-impact-of-googles-new-chrome-security-policy-on-webrtc/
+// const serverConfig = {
+//     key: fs.readFileSync('key.pem'),
+//     cert: fs.readFileSync('cert.pem'),
+// };
 
-            case TYPE_REQUEST_OFFER:
-                wss.broadcast(message);
-                break;
-        }
-    });
+// // Create a server for the client html page
+// var handleRequest = function(request, response) {
+//     console.log("Trying to serve : " + request.url);
+//     if (request.url === '/') {
+//         response.writeHead(200, { 'Content-Type': 'text/html' });
+//         response.end(fs.readFileSync('../client/login.html'));
+//     } else if (request.url === '/app.js') {
+//         response.writeHead(200, { 'Content-Type': 'application/javascript' });
+//         response.end(fs.readFileSync('./client/app.js'));
+//     } else if (request.url === '/app') {
+//         response.writeHead(200, { 'Content-Type': 'text/html' });
+//         response.end(fs.readFileSync('./client/index.html'));
+//     } else if (request.url === "/style.css") {
+//         response.writeHead(200, { 'Content-Type': 'text/css' });
+//         response.end(fs.readFileSync('./client/style.css'));
+//     } else if (request.url === "/img/slide_01.jpg") {
+//         response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+//         response.end(fs.readFileSync('./client/img/slide_01.jpg'));
+//     } else if (request.url === "/img/slide_02.jpg") {
+//         response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+//         response.end(fs.readFileSync('./client/img/slide_01.jpg'));
+//     } else if (request.url === "/img/logo.png") {
+//         response.writeHead(200, { 'Content-Type': 'image/png' });
+//         response.end(fs.readFileSync('./client/img/logo.png'));
+//     } else if (request.url === "/img/HeaderImg.png") {
+//         response.writeHead(200, { 'Content-Type': 'image/png' });
+//         response.end(fs.readFileSync('./client/img/HeaderImg.png'));
+//     } else if (request.url === "/img/Logo_Clear2.png") {
+//         response.writeHead(200, { 'Content-Type': 'image/png' });
+//         response.end(fs.readFileSync('./client/img/Logo_Clear2.png'));
+//     } else if (request.url === "/face.js") {
+//         response.writeHead(200, { 'Content-Type': 'application/javascript' });
+//         response.end(fs.readFileSync('./client/lib/face-min.js'));
+//     } else if (request.url === "/tracking.js") {
+//         response.writeHead(200, { 'Content-Type': 'application/javascript' });
+//         response.end(fs.readFileSync('./client/lib/tracking-min.js'));
+//     } else {
+//         console.log("Unknown path " + request.url);
+//         response.writeHead(404);
+//         response.end();
+//     }
+// };
 
-    ws.on('close', function(code) {
-        //delete clients
-        console.log("Connection closed by " + ws + " with code " + code);
-    });
-});
+// var httpsServer = https.createServer(handleRequest);
+// httpsServer.listen(HTTPS_PORT);
+// httpsServer.timeout = 4000;
+// // Create a server for handling websocket calls
+// var wss = new WebSocket.Server({
+//     server: httpsServer,
+//     clientTracking: true,
+// });
 
-wss.broadcast = function(data) {
-    var sender = (data.id) ? data.id : "unknown_id";
-    this.clients.forEach(function(client) {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(data);
-        }
-    });
-};
+// wss.on('connection', function(ws) {
+//     ws.on('message', function(message) {
+//         var msg = JSON.parse(message);
+//         switch (msg.type) {
+//             case TYPE_INITIAL_HANDSHAKE:
+//                 //clients[msg.id] = ws;
+//                 console.log("New handshake from " + msg.id + " at " + msg.date);
+//                 break;
 
-function onIncomingMessage(message) {
-    var msg = JSON.parse(message);
-    switch (msg.type) {
-        case TYPE_INITIAL_HANDSHAKE:
-            //clients[] = 
-            console.log("New handshake from " + msg.id + " at " + msg.date);
-            break;
-    }
-}
+//             case TYPE_SDP_CONNECTION:
+//                 wss.broadcast(message); //todo - sent to the correct client 
+//                 break;
 
-console.log('Server initialized and running on https://localhost:' + HTTPS_PORT);
+//             case TYPE_ICE_INFO:
+//                 wss.broadcast(message);
+//                 break;
+
+//             case TYPE_BITRATE_CHANGED_INFO:
+//                 wss.broadcast(message);
+//                 break;
+
+//             case TYPE_CHAT_MESSAGE:
+//                 wss.broadcast(message);
+//                 break;
+
+//             case TYPE_REQUEST_OFFER:
+//                 wss.broadcast(message);
+//                 break;
+//         }
+//     });
+
+//     ws.on('close', function(code) {
+//         //delete clients
+//         console.log("Connection closed by " + ws + " with code " + code);
+//     });
+// });
+
+// wss.broadcast = function(data) {
+//     var sender = (data.id) ? data.id : "unknown_id";
+//     this.clients.forEach(function(client) {
+//         if (client.readyState === WebSocket.OPEN) {
+//             client.send(data);
+//         }
+//     });
+// };
+
+// function onIncomingMessage(message) {
+//     var msg = JSON.parse(message);
+//     switch (msg.type) {
+//         case TYPE_INITIAL_HANDSHAKE:
+//             //clients[] = 
+//             console.log("New handshake from " + msg.id + " at " + msg.date);
+//             break;
+//     }
+// }
+
+// console.log('Server initialized and running on https://localhost:' + HTTPS_PORT);
